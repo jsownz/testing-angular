a27f186e4f5205429898041e42af1c125826df89
Jason Sohl <jason@frozensoliddesigns.com>
2014-07-27 21:15:38 -0700

    adding test for postgres and Procfile for heroku

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var config = require('../private/config');
+
+var pg = require('pg').native,
+    connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/'+config.PG_DATABASE,
+    start = new Date(),
+    client;
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -8,7 +14,28 @@ router.get('/', function(req, res) {
 
 /* GET test page. */
 router.get('/test', function(req, res){
-  res.render('test', { title: 'Test Page' });
+
+  client = new pg.Client(connectionString);
+  client.connect();
+
+  var date = new Date(),
+      title = 'Test Page with psql';
+
+  client.query('INSERT INTO visits(date) VALUES($1)', [date]);
+
+  query = client.query('SELECT COUNT(date) AS count FROM visits WHERE date = $1', [date]);
+  query.on('row', function(result) {
+    console.log(result);
+
+    if (!result) {
+      return res.render('test', { title: title, result: 'no results' });
+    } else {
+      res.render('test', { title: title, result: 'Visits today: ' + result.count });
+    }
+  });
+
+  query.on('end', function() { client.end(); });
+
 });
 
 module.exports = router;
