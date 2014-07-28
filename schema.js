0588787acaf09a581fbc4614d825cdb76b73bacc
Jason Sohl <jason@frozensoliddesigns.com>
2014-07-27 21:23:35 -0700

    update schema.js

--- schema.js
+++ schema.js
@@ -1,8 +1,7 @@
 var pg = require('pg').native,
-    config = require('./private/config'),
-  connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/'+config.PG_DATABASE,
-  client,
-  query;
+    connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/angular',
+    client,
+    query;
 
 client = new pg.Client(connectionString);
 client.connect();
