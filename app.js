3ae48f178d9f512108546609efebea16c0594dde
Jason Sohl <jason@frozensoliddesigns.com>
2014-07-31 16:40:05 -0700

    added pusherjs, updated gitignore

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var sass = require('node-sass');
 var passport = require('passport');
+var Pusher = require('pusher');
 
 var routes = require('./routes/index');
 var apiRoutes = require('./routes/api');
